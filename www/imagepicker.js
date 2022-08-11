b0f1a918df32f18644e0631f9ca25600f1401ea6
Paul NOH <fireonmac@gmail.com>
2022-08-11 10:47:49 +0900

    add ios title tranlsation in ko vi

--- www/imagepicker.js
+++ www/imagepicker.js
@@ -53,13 +53,26 @@ ImagePicker.prototype.getPictures = function(success, fail, options) {
 
 	this.validateOutputType(options);
 
+	var getAlbumTitle = function() {
+		var locale = window.navigator.language.slice(0, 2);
+
+		switch (locale) {
+			case 'ko':
+				return '앨범 선택'
+			case 'vi':
+				return 'Chọn một Anbom'
+			default:
+				return 'Select an Album';			
+		}
+	}
+
 	var params = {
 		maximumImagesCount: options.maximumImagesCount ? options.maximumImagesCount : 15,
 		width: options.width ? options.width : 0,
 		height: options.height ? options.height : 0,
 		quality: options.quality ? options.quality : 100,
 		allow_video: options.allow_video ? options.allow_video : false,
-		title: options.title ? options.title : 'Select an Album', // the default is the message of the old plugin impl
+		title: options.title ? options.title : getAlbumTitle(), // the default is the message of the old plugin impl
 		message: options.message ? options.message : null, // the old plugin impl didn't have it, so passing null by default
 		outputType: options.outputType ? options.outputType : this.OutputType.FILE_URI,
 		disable_popover: options.disable_popover ? options.disable_popover : false // Disable the iOS popover as seen on iPad
